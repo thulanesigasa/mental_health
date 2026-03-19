f34e028e4602e80271b1b9b67710de3a9797cc40
thulanesigasa <thsigeec025@student.wethinkcode.co.za>
2026-03-19 16:20:49 +0200

    Add core frontend templates, CSS, and configure CSP security headers

--- app/__init__.py
+++ app/__init__.py
@@ -14,6 +14,25 @@ def create_app(config_class=Config):
     # Import models so Alembic can discover them
     from app import models
 
-    # TODO: Register blueprints
+    # Register blueprints
+    from app.views.main import bp as main_bp
+    app.register_blueprint(main_bp)
+
+    @app.after_request
+    def set_secure_headers(response):
+        # Strict Content Security Policy
+        csp = (
+            "default-src 'self'; "
+            "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; "
+            "font-src 'self' https://fonts.gstatic.com; "
+            "img-src 'self' data:; "
+            "script-src 'self'; "
+            "frame-ancestors 'none';"
+        )
+        response.headers['Content-Security-Policy'] = csp
+        response.headers['X-Content-Type-Options'] = 'nosniff'
+        response.headers['X-Frame-Options'] = 'DENY'
+        response.headers['X-XSS-Protection'] = '1; mode=block'
+        return response
 
     return app
