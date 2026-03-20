f9f320b3fe858260488bdfc04093e207f9058925
thulanesigasa <thsigeec025@student.wethinkcode.co.za>
2026-03-20 08:20:16 +0200

    Implement Phase 3 UI/UX enhancements with pure CSS glassmorphic animations, progress tracking, and interactive metadata

--- app/views/modules.py
+++ app/views/modules.py
@@ -1,15 +1,19 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, session
 from app.models.module import Module
+from app.models.user import User
 
 bp = Blueprint('modules', __name__, url_prefix='/modules')
 
 @bp.route('/')
 def list_modules():
-    modules = Module.query.order_by(Module.created_at.desc()).all()
-    # If no modules, we can create some dummy ones or just render empty
-    return render_template('modules/list.html', modules=modules)
+    modules = Module.query.order_by(Module.id.asc()).all()
+    user = None
+    if 'user_id' in session:
+        user = User.query.get(session['user_id'])
+    return render_template('modules/list.html', modules=modules, user=user)
 
 @bp.route('/<int:module_id>')
 def detail(module_id):
     module = Module.query.get_or_404(module_id)
-    return render_template('modules/detail.html', module=module)
+    next_module = Module.query.filter(Module.id > module_id).order_by(Module.id.asc()).first()
+    return render_template('modules/detail.html', module=module, next_module=next_module)
